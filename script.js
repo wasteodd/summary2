f8b996a0618a56c707ef2ba284d2a1e9aea2f420
y3w <jakkitsme@gmail.com>
2025-10-24 08:34:08 -0700

    white board with out anything

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
 const board = document.querySelector('#board');
 const ctx = board.getContext("2d");
 
+let drawing = false;
+let currnetColor = "black";
 
+board.addEventListener("mousedown", (e) => {
+    drawing = true;
+    ctx.beginPath();
+    ctx.moveTo(e.offsetX, e.offsetY);
+});
+
+
+board.addEventListener("mousemove", (e) => {
+    if (drawing) {
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.strokeStyle = currnetColor;
+        ctx.lineWidth = 3;
+        ctx.lineCap = "round";
+        ctx.stroke();
+    }
+});
+
+
+board.addEventListener("mouseup", (e) => {
+    drawing = false;
+    ctx.closePath();
+});
+
+
+board.addEventListener('mouseout', () =>{
+    drawing = false;
+    ctx.closePath();
+})
+
+const colorOption = document.querySelectorAll(".color-option");
+colorOption.forEach(option =>{
+    option.addEventListener("click" , () => {
+        currnetColor = option.getAttribute('data-color');
+        console.log(currnetColor)
+    });
+});
